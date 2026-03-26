da4c33f5dd7f45bb059b6c57f622be56bf843667
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2026-03-26 19:55:04 +0900

    fix: fix static to inline

--- cxx/include/aa_registory.hh
+++ cxx/include/aa_registory.hh
@@ -43,7 +43,7 @@ namespace aa_registry {
     ParsedConfig parse_config(const nlohmann::json& j);
 
 #ifndef TEST
-    static ParsedConfig g_config;
+    inline ParsedConfig g_config;  // Global cache;
 
     inline void OnLoaded() {
         const char*   CONFIG_PATH = "Data/SKSE/Plugins/fnis_aa/config.json";
